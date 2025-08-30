d983d32cf8e6cd19734d76818d6460c983c049c3
jits98 <jits8809@gmail.com>
2025-08-31 00:25:41 +0530

    add functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 100 + 1);
+
+  if (randomNumber < 34) {
+    console.log("rock");
+  } else if (randomNumber < 68) {
+    console.log("paper");
+  } else if (randomNumber < 100) {
+    console.log("scissors");
+  }
+}
+
+function getHumanChoice() {
+  const userInput = prompt(
+    "Please write one of the following 'rock', 'paper' & 'scissor' ",
+    ""
+  );
+
+  console.log(userInput);
+}
+
+const humanScore = 0;
+
+const computerScore = 0;
